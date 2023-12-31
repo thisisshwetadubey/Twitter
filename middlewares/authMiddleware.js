46f25d399b6a80dd050c7b6fed70856bbbf34cdb
thisisshwetadubey <dubeyshweta7049@gmail.com>
2023-12-31 23:27:53 +0530

    login functionality added

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,2 +1,36 @@
-const  jwt = require('jsonwebtoken')
-const use
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+class protect {
+  async process(req, res, next) {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      try {
+        token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById({ _id: decoded.id }).select(
+          "-password"
+        );
+        next();
+      } catch (error) {
+        res.status(401).json({
+          statusCode: "401",
+          type: "Error",
+          data: "Unauthorized User",
+        });
+      }
+    }
+    if (!token) {
+      res.status(401).json({
+        statusCode: "401",
+        type: "error",
+        data: "No token, Unauthorized User",
+      });
+    }
+  }
+}
+module.exports = new protect();
