5656e7b817bb06af8f496ff1ef242a4c297eb1ad
thisisshwetadubey <dubeyshweta7049@gmail.com>
2023-12-31 17:15:55 +0530

    verifyOTP functionality

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.post("/signup",require("../controllers/user/signup").process); // Register User
+router.post("/signup", require("../controllers/user/signup").process); // Register User
+router.post("/verify", require("../controllers/user/verify").process);
 
 module.exports = router;
