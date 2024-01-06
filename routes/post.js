513b3e1ba836d95577f25b2edd97f35db29493de
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-06 22:00:45 +0530

    added middleware in post API

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
+const protect = require("../middlewares/authMiddleware").process
 
-router.post("/create", require("../controllers/post/createposts").process)
-router.get("/get/:id", require("../controllers/post/getPost").process)
-router.delete("/delete/:id", require("../controllers/post/deletepost").process)
-router.put("/update", require("../controllers/post/updatepost").process)
+router.post("/create", protect,require("../controllers/post/createposts").process)
+router.get("/get/:id", protect,require("../controllers/post/getPost").process)
+router.delete("/delete/:id", protect,require("../controllers/post/deletepost").process)
+router.put("/update", protect,require("../controllers/post/updatepost").process)
 
 
 module.exports = router
