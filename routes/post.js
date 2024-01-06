a3edbaa242e03776bb497dfe5f5dcf35f6013dd5
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-06 21:40:04 +0530

    delete post API

--- routes/post.js
+++ routes/post.js
@@ -3,5 +3,6 @@ const router = express.Router()
 
 router.post("/create", require("../controllers/post/createposts").process)
 router.get("/get/:id", require("../controllers/post/getPost").process)
+router.delete("/delete/:id", require("../controllers/post/deletepost").process)
 
 module.exports = router
