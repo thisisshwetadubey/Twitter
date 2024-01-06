70e4e553a5b3afc4c29e94f67fd988de6ce7be58
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-06 21:13:26 +0530

    get post API

--- routes/post.js
+++ routes/post.js
@@ -2,5 +2,6 @@ const express = require("express")
 const router = express.Router()
 
 router.post("/create", require("../controllers/post/createposts").process)
+router.get("/get/:id", require("../controllers/post/getPost").process)
 
 module.exports = router
