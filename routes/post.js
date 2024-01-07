449877d63bc61f5a1fcb0beddc31591d37f6a245
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-07 13:44:10 +0530

    added post API for fetching all posts

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const protect = require("../middlewares/authMiddleware").process
 
 router.post("/create", protect,require("../controllers/post/createposts").process)
-router.get("/get/:id", protect,require("../controllers/post/getPost").process)
+router.get("/", protect,require("../controllers/post/getAllpost").process)
+router.get("/get/:id", protect,require("../controllers/post/getpost").process)
 router.delete("/delete/:id", protect,require("../controllers/post/deletepost").process)
 router.put("/update", protect,require("../controllers/post/updatepost").process)
 
