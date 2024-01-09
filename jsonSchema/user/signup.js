ab4a8725b94beb9a8194ff1797c52f599c9ccc18
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-09 11:27:40 +0530

    signup changes

--- jsonSchema/user/signup.js
+++ jsonSchema/user/signup.js
@@ -1,13 +1,16 @@
 module.exports = {
   type: "object",
+  additionalProperties: false,
   properties: {
     name: { type: "string", required: true },
+    username: { type: "string", required: true },
     email: {
       type: "string",
       pattern:
         '^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$',
       required: true,
     },
-    password :{type: "string", required: true}
+    password: { type: "string", required: true },
+    isGoogleAuth: { type: "boolean" },
   },
 };
