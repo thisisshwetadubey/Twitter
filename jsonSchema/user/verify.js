612dd508dff8ae2fd6aa37854efcf2264c679961
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-09 11:45:28 +0530

    verify otp

--- jsonSchema/user/verify.js
+++ jsonSchema/user/verify.js
@@ -2,11 +2,14 @@ module.exports = {
   type: "object",
   additionalProperties: false,
   required: true,
-  email: {
-    type: "string",
-    pattern:
-      '^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$',
-    required: true,
-  },
-  otp: { type: "string", required: true },
+  properties:{
+    email: {
+      type: "string",
+      pattern:
+        '^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$',
+      required: true,
+    },
+    otp: { type: "string", required: true },
+  }
+  
 };
