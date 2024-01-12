ccfda9e279d7a73faa55cda7d8d1cd7084210e72
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-12 15:49:33 +0530

    get Specific post API

--- routes/post.js
+++ routes/post.js
@@ -4,9 +4,10 @@ const protect = require("../middlewares/authMiddleware").process
 
 router.post("/create", protect,require("../controllers/post/createposts").process)
 router.get("/", protect,require("../controllers/post/getAllpost").process)
-router.get("/get/:id", protect,require("../controllers/post/getpost").process)
+router.get("/get/:id", protect,require("../controllers/post/getUserPosts").process)
 router.delete("/delete/:id", protect,require("../controllers/post/deletepost").process)
 router.put("/update", protect,require("../controllers/post/updatepost").process)
+router.get("/getPost/:id", protect,require("../controllers/post/getPost").process)
 
 
 module.exports = router
