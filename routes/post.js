1b25b2653d04409368ba328e94bbdecdb36d06b9
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-12 15:38:05 +0530

    getall and specific comment API

--- routes/post.js
+++ routes/post.js
@@ -10,6 +10,11 @@ router.put("/update", protect,require("../controllers/post/updatepost").process)
 router.post("/isLike/:id", require("../controllers/post/isLike").process)
 router.post("/isRetweet/:id", require("../controllers/post/isRetweet").process)
 router.post("/isBookmark/:id", require("../controllers/post/isBookmark").process)
+router.post("/createComment", require("../controllers/comments/create").process)
+router.get("/comment", require("../controllers/comments/getAllComments").process)
+router.get("/comment/:id", require("../controllers/comments/getComment").process)
+
+
 
 
 module.exports = router
