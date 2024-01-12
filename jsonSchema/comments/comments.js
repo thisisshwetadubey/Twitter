4121612ec16dbf2d5b574c18a548d950d41fd52a
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-12 15:37:46 +0530

    post comment jsonschema

---
+++ jsonSchema/comments/comments.js
@@ -0,0 +1,10 @@
+module.exports = {
+  type: "Object",
+  additionalProperties: false,
+  required: true,
+  properties: {
+    comments: { type: "string", required: true },
+    postId: { type: "string", required: true },
+    userId: { type: "string" },
+  },
+};
