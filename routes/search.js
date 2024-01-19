198be5863d5ea4c02cecafcd9b8dd6205b73a1d1
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-19 11:53:11 +0530

    Trending sports API

--- routes/search.js
+++ routes/search.js
@@ -5,3 +5,8 @@ const protect = require("../middlewares/authMiddleware").process
 
 
 router.get("/trending",protect,require("../controllers/search/trending").process)
+router.get("/trendingNews", protect, require("../controllers/search/newsTrending").process)
+router.get("/trendingSports", protect, require("../controllers/search/sportsEntertainment").process)
+
+
+module.exports = router
