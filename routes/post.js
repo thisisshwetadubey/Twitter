a1ef76f346212419e68d59d3b03ccbe5d5a46c4c
thisisshwetadubey <dubeyshweta7049@gmail.com>
2024-01-20 20:03:52 +0530

    Edit profile Posts changes

--- routes/post.js
+++ routes/post.js
@@ -4,7 +4,6 @@ const protect = require("../middlewares/authMiddleware").process
 
 router.post("/create", protect,require("../controllers/post/createposts").process)
 router.get("/", protect,require("../controllers/post/getAllpost").process)
-router.get("/get/:id", protect,require("../controllers/post/getUserPosts").process)
 router.delete("/delete/:id", protect,require("../controllers/post/deletepost").process)
 router.put("/update", protect,require("../controllers/post/updatepost").process)
 router.get("/getPost/:id", protect,require("../controllers/post/getPost").process)
