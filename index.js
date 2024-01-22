bb996ef80f906bd1fb97e6cf25b12328b27b672f
thisisrahulpal <rahulgurupal@gmail.com>
2024-01-22 18:01:26 +0530

    configuration

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const MongoDb = require("./config/db");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5173"
+      : process.env.ClIENT_URL,
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 const errorHandler = require("./middlewares/errorHandler").error;
 
 app.use(cookieParser());
-app.use(cors());
 app.use(bodyParser.json());
 MongoDb.connectDB();
 
--- util/setToken.js
+++ util/setToken.js
@@ -2,17 +2,16 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (res, id) => {
   try {
-    const token =  jwt.sign({ userId: id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
+    const token = jwt.sign({ userId: id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
     });
-    
+
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
       sameSite: "strict",
       age: 24 * 60 * 60 * 1000,
     });
-    
   } catch (error) {
     throw "Unable to create token";
   }
